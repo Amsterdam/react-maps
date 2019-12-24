782602e7403ff7f16644e115bb6c39bd16d3ae28
Chris van Mook <chris@chrisvanmook.com>
2019-12-24 10:56:40 +0100

    Wire leaflet events to Marker

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -1,21 +1,30 @@
 import React, { useEffect } from 'react'
 import L, {
   LatLngExpression,
+  LeafletEventHandlerFn,
   Marker as MarkerType,
   MarkerOptions,
 } from 'leaflet'
 import useMapInstance from './useMapInstance'
+import useLeafletEvents from './useLeafletEvents'
 
 type Props = {
   latlng: LatLngExpression
   options?: MarkerOptions
+  events?: { [key: string]: LeafletEventHandlerFn }
 }
 
-const Marker: React.FC<Props> = ({ latlng, options }) => {
+const Marker: React.FC<Props> = ({ latlng, events, options }) => {
   const [markerInstance, setMarkerInstance] = React.useState<MarkerType | null>(
     null,
   )
   const { mapInstance } = useMapInstance()
+
+  useEffect(() => {
+    if (markerInstance) {
+      useLeafletEvents(markerInstance, events)
+    }
+  }, [markerInstance])
 
   // add marker
   useEffect(() => {
