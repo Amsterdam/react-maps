fe02ecae2d8e9680dbded7b4a67410a4ba2bd010
Jon Koops <jonkoops@gmail.com>
2020-12-22 19:17:29 +0100

    Fix deprecation errors for Jest

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.json',
     },
   },
   testRegex: ['/__tests__/.*\\.(ts|tsx|js)$', '/*.test\\.(ts|tsx|js)$'],
