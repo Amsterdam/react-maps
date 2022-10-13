e2adec7918ec986a78dcad8579467f60046d8d6f
Jasper Swart <jasper.g.swart@gmail.com>
2022-10-13 14:16:14 +0200

    added typescript

--- src/example/example.tsx
+++ src/example/example.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-const container = document.getElementById('root') as any
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
+const container = document.getElementById('root')
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!)
 root.render(<App />)
